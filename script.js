92506d54c495c39ec08137cb0a8040be37e52b84
Andreigeorge2 <160678106+Andreigeorge2@users.noreply.github.com>
2024-06-10 13:29:14 +0300

    Variabile  javascript

---
+++ script.js
@@ -0,0 +1,26 @@
+// declaim variable var let si const 
+
+//let myNumber=2;
+//console.log(myNumber);
+
+//myNumber=10;
+//console.log(myNumber);
+
+//const age=20
+//console.log (age);
+
+//let message;
+//console.log(message);
+
+// Reguli de denumire  
+//1. numele poate sa contina doar litere, cifre, _sau $
+//2.numele nu poate incepe cu o cifra, nu contine spatii 
+// sau cuvinte
+
+{
+let firstName = "Andrei";
+console.log(firstName); // Logs "Andrei"
+
+console.log(firstName); // Logs "Andrei" again
+
+}
