913885e5a0e42593e33b9578d67598e4556f3fc6
Andreigeorge2 <160678106+Andreigeorge2@users.noreply.github.com>
2024-06-12 11:42:15 +0300

    while do while and for exercise

--- script.js
+++ script.js
@@ -1,24 +1,61 @@
-// let firstName='Alex';
-// let lastName="Pop";
+// practice javascript exercise 
+let myArray=["Ana","Mihai", "Alex", "Marius"];
+let i=0;
+do {
+    console.log(myArray[i]);
+    i++;
+} while (i <= myArray.length-1)
 
-// console.log(firstName);
 
-// console.log(firstName[3]);
 
-// console.log(firstName.length);
 
-// let fullName=firstName+ "  " + lastName;
 
-// console.log(fullName);
 
-// console.log(lastName.indexOf("o"));
 
-// let message="Acesta este un mesaj";
-// let arr=message.split(" ")
-// let newMessage=arr.join("= ")
-// console.log(newMessage);
 
-let firstName = "Andrei";
-let lastName = "George";
-let message = `Numele meu este: ${firstName} ${lastName}`;
-console.log(message);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
